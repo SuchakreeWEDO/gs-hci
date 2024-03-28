13b150a2e2a407ef2a6d03c9e60180040cce1b11
chanapap <chanapap@scg.com>
2024-03-28 11:00:17 +0700

    update fb_render_ref.py

--- fb_render_ref.py
+++ fb_render_ref.py
@@ -61,3 +61,5 @@ if __name__ == "__main__":
     print("Rendering " + args.model_path)
 
     render_sets(model.extract(args), args.iteration, pipeline.extract(args))
+
+# python fb_render_novelview.py -m D:\3d-reconstruction\datasets\scale_figure
